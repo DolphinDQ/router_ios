89b6bb79af7c5453027ae45d31bdcdd244cfd455
Sphynx <229860255@qq.com>
2016-02-01 09:00:59 +0800

    first commit

---
+++ router_iosTests/router_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  router_iosTests.swift
+//  router_iosTests
+//
+//  Created by mr-tech on 16/2/1.
+//  Copyright © 2016年 mrtech. All rights reserved.
+//
+
+import XCTest
+@testable import router_ios
+
+class router_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
